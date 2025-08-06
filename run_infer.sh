3ea42b615476d2bd7ce8cddcc86a5fccd707724f
weiran0129 <wwr0129@gmail.com>
2025-08-06 12:29:31 -0400

    Remove unused files and scripts

--- run_infer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python inference.py \
-  --model_ckpt src/model/check_points/cvae_tacotron2_trial1.pth \
-  --tacotron_ckpt src/model/check_points/tacotron2_pretrained.pt \
-  --text "Hello world" \
-  # --speaker_id p323 \
-  --out_wav ./demo_result/demo.wav
--- tacotron_pretrained_weights_loading.py
+++
@@ -1,9 +0,0 @@
-import torch
-# Load the pretrained Tacotron2 model from NVIDIA's repository
-tacotron2 = torch.hub.load(
-    'NVIDIA/DeepLearningExamples:torchhub',
-    'nvidia_tacotron2',
-    pretrained=True).eval()
-
-torch.save(tacotron2.state_dict(), 'tacotron2_pretrained.pt')
-print("saved -> tacotron2_pretrained.pt")
